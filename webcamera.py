a599b64b92be8f83eb0a941a51a892bf1c2afbd8
osakazzang <132719734+osakazzang@users.noreply.github.com>
2026-05-16 14:45:39 +0900

    Update webcamera.py

--- webcamera.py
+++ webcamera.py
@@ -54,7 +54,7 @@ st.markdown(hide_st_style, unsafe_allow_html=True)
 # 1. アプリのメインロジック
 # ==========================================
 GAS_URL = st.secrets["GAS_URL"]
-SECRET_TOKEN = st.secrets.get("SECRET_TOKEN", "my_secret_token_123")
+SECRET_TOKEN = st.secrets.get("SECRET_TOKEN", "my_secret_token_$0656$")
 
 st.title("👕 衣類データ登録")
 
